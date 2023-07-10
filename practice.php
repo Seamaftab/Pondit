1f7d6d435aee276afb29fc082a08b399a9ee35a0
Seam Aftab <flynnsam1997@gmail.com>
2023-07-10 20:05:20 +0600

    new

--- practice.php
+++ practice.php
@@ -6,8 +6,44 @@
     <title>Practice - Class 3</title>
 </head>
 <body>
+
     <?php
-    echo "hello"
+        $n = 0;
+        $number = 0;
+        while ($number<20) {
+            $n++;
+            $number = $n;
+            //echo $number."<br>";   
+        }
+        echo "The number finally is : ".$number;
+        echo "<br>Let's Create a table, shall we?<br>";
+        function table($number)
+        {
+            for($i=1;$i<=10;$i++)
+            {
+                print $number." x ".$i." = ".$i*$number."<br>";
+            }
+        }
+
+        table($number);
+        echo "<br><br>";
+        print("time for some string operations<br>shall we?<br>");
+
+        function Trying($name, $Age) //Default age
+        {
+            echo "My name is $name and my age is $Age<br>";
+        }
+        Trying("Seam", 25);
+        Trying("Habib", 20);
+    ?>
+
+    <?php
+        echo "Time for array<br>";
+
+        $cars = ['Lambo','Ferrari','Porsche'];
+
+        echo '<pre>';
+        print_r($cars);
     ?>
 </body>
 </html>
