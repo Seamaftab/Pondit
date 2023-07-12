3349169e46c282a1f319956a538c06fb0ccd4e57
Seam Aftab <flynnsam1997@gmail.com>
2023-07-12 23:53:27 +0600

    class 4 work completed

--- class_4.php
+++ class_4.php
@@ -20,6 +20,78 @@
 				echo "&nbsp;&nbsp;&nbsp;".($k+1)." = ".$v."<br>";
 			}
 		}
+
+		//Function
+
+		function add($a,$b)
+		{
+			return $a+$b;
+		}
+		function sub($a,$b)
+		{
+			if($a>$b)
+				return $a-$b;
+			else
+				return $b-$a;
+		}
+		function mult($a,$b)
+		{
+			return $a*$b;
+		}
+		function div($a,$b)
+		{
+			if($a>$b)
+				return $a/$b;
+			else
+				return $b/$a;
+		}
+		function mod($a,$b)
+		{
+			if($a>$b)
+				return $a%$b;
+			else
+				return $b%$a;
+		}
+
+		$number_1 = 22;
+		$number_2 = 7;
+
+		echo "<br>Operations for $number_1 and $number_2 are : <br><br>";
+
+		echo "Addition : ".add($number_1,$number_2)."<br>";
+		echo "Subtraction : ".sub($number_1,$number_2)."<br>";
+		echo "Multiplication : ".mult($number_1,$number_2)."<br>";
+		echo "Division : ".div($number_1,$number_2)." with modulas being ".mod($number_1,$number_2)."<br><br>";
+
+		//Array String Functions
+
+		$phones = ['Nokia', 'Blackberry', 'Motorolla', 'Samsung', 'Redmi', 'Realme','OnePlus', 'Vivo', 'Apple'];
+
+		foreach ($phones as $key => $value) {
+			echo ($key+1)." => $value<br>";
+		}
+
+		echo "size of the array : ",sizeof($phones),"<br>";
+
+		sort($phones);
+		echo "<br>This shows the array as in alphabatical ascending order : <br>";
+		foreach ($phones as $key => $value) {
+			echo ($key+1)." => $value<br>";
+		}
+
+		rsort($phones);
+		echo "<br>This shows the array as in alphabatical decending order : <br>";
+		foreach ($phones as $key => $value) {
+			echo ($key+1)." => $value<br>";
+		}
+
+		print("<br><br>");
+		//String Operations (functions)
+
+		$str = "fname:Seam lname:Aftab";
+		sscanf($str,"fname:%s lname:%s",$fname,$lname);
+		// show types and values
+		var_dump($fname,$lname);
 	?>
 </body>
 </html>
